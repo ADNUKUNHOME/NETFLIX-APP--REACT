80e18e8cc92b6472b8cb4fd01ccd16037fc9c315
Muhammad Adnan <adnukunhome7@gmail.com>
2024-11-14 07:44:36 -0800

    Added a Top Scroll Button.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,34 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import Navbar from './Components/Navbar/Navbar';
 import RowPost from './Components/RowPost/RowPost';
+import { useState, useEffect } from 'react';
 import { originals, action, trending, horror, comedy, romance } from './urls';
 
 function App() {
+
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
     <div>
       <Navbar/>
@@ -15,6 +40,12 @@ function App() {
       <RowPost  url={horror}  title='Horror' isSmall />
       <RowPost  url={comedy}  title='Comedy' isSmall />
       <RowPost  url={romance}  title='Romance' isSmall />
+
+      {showScrollButton && (
+        <button onClick={scrollToTop} className="scrollToTop">
+          ↑
+        </button>
+      )}
     </div>
   );
 }
